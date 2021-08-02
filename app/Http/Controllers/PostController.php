67af4f72a895c7c66e02f08f5cd631ba08f12813
Rikuo1207 <rikuo1207@outlook.jp>
2021-08-02 17:07:05 +0900

    third commit

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,20 +2,14 @@
 
 namespace App\Http\Controllers;
 
-<<<<<<< HEAD
 use App\Post;
-=======
->>>>>>> 3e9ddc3500dd7a100354483fd46fee6606a27e9c
+
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-<<<<<<< HEAD
     public function index(Post $post)
     {
         return $post->get();
     }
-=======
-    //
->>>>>>> 3e9ddc3500dd7a100354483fd46fee6606a27e9c
 }
